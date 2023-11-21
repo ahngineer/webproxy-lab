b4fb61219b8df4a8b2b26005bfea12f4c11764e6
ahngineer <ahngineer@gmail.com>
2023-11-21 14:58:11 +0900

    delete doodle

--- doodle/connect_bind_listen_accept.c
+++
@@ -1,8 +0,0 @@
-#include "csapp.h"
-
-// client side, 0 OK, -1 ERROR
-int connect (int clientfd, const struct sockaddr *addr, socklen_t addrlen);
-
-// server side, 0 OK, -1 ERROR 
-int bind (int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-// int listen ()
--- doodle/socket_함수.c
+++
@@ -1,7 +0,0 @@
-#include "csapp.h"
-
-// nonnegative descriptor if OK,
-// 01 on error
-int socket(int domain, int type, int protocol);
-
-clientfd = socket(AF_INET, SOCK_STREAM, 0);
